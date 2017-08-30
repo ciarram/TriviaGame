24d10fd7f1edf618be1c0f88e63d4ed4533654c2
Ciarra Murphy <mciarra@ymail.com>
2017-08-29 23:05:48 -0400

    added question arrays

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,35 @@
 window.onload = function(){
     //start button that when clicked will display the first question
-    //$(".start-game").click
+    //$(".start-game").on('click', function());
+
 }
 
 //Timer
+var firstQuestion = {
+    question1: "Who wrote Pride and Prejudice?",
+    answer: "Jane Austen",
+    firstFalseAnswer: "J.K. Rowling",
+    secondFalseAnswer: "Stephen King",
+
+};
+
+var secondQuestion = {
+    question2: "Which Charles Dickens work has over 100 movie adaptations?",
+    answer: "A Christmas Carol",
+    firstFalseAnswer: "Oliver Twist",
+    secondFalseAnswer: "Great Expectations",
+};
+
+var thirdQuestion = {
+    question3: "When was the first Harry Potter book published?",
+    answer: "1997",
+    firstFalseAnswer: "2001",
+    secondFalseAnswer: "2010",
+
+};
+var fourthQuestion = {
+    question4: "What is Florida author Zora Neale Hurston\s most famous work?",
+    answer: "Their Eyes Were Watching God",
+    firstFalseAnswer: "Jonah's Gourd Vine",
+    secondFalseAnswer: "Mules and Men",
+}
