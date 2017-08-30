fe807c4fc23c1621ce2f1c347816e7598c78cd49
Ciarra Murphy <mciarra@ymail.com>
2017-08-30 16:26:37 -0400

    tested objects in js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,6 +10,7 @@ var firstQuestion = {
     answer: "Jane Austen",
     firstFalseAnswer: "J.K. Rowling",
     secondFalseAnswer: "Stephen King",
+
     //function
         //if (userGuess == answer) {
             //console.log("Correct!");
@@ -20,6 +21,8 @@ var firstQuestion = {
             //console.log("sorry, time's up");
         //}
 };
+    console.log(firstQuestion.question1);
+    console.log(firstQuestion.answer);
 
 var secondQuestion = {
     question2: "Which Charles Dickens work has over 100 movie adaptations?",
@@ -36,6 +39,8 @@ var secondQuestion = {
             //console.log("sorry, time's up");
         //}
 };
+    console.log(secondQuestion.question2);
+    console.log(secondQuestion.firstFalseAnswer);
 
 var thirdQuestion = {
     question3: "When was the first Harry Potter book published?",
@@ -52,6 +57,8 @@ var thirdQuestion = {
             //console.log("sorry, time's up");
         //}
 };
+    console.log(thirdQuestion.question3);
+    console.log(thirdQuestion.secondFalseAnswer);
 var fourthQuestion = {
     question4: "What is Florida author Zora Neale Hurston\s most famous work?",
     answer: "Their Eyes Were Watching God",
@@ -67,3 +74,7 @@ var fourthQuestion = {
             //console.log("sorry, time's up");
         //}
 }
+console.log(fourthQuestion.question4);
+console.log(fourthQuestion.answer);
+console.log(fourthQuestion.firstFalseAnswer);
+console.log(fourthQuestion.secondFalseAnswer);
