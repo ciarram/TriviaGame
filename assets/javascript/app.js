4e0c5cb839a0f13c2b117fd6a68fb156f69fe700
Ciarra Murphy <mciarra@ymail.com>
2017-08-30 17:25:56 -0400

    attempting to link answers in object to answer buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,45 @@
-window.onload = function(){
+$(document).ready(function(){
     //start button that when clicked will display the first question
     //$(".start-game").on('click', function());
 
-}
-
 //Timer
+
+//Variables
+var win = 0;
+var loss = 0;
+var unanswered = 0;
+var userGuess;
+
+//Questions
 var firstQuestion = {
     question1: "Who wrote Pride and Prejudice?",
-    answer: "Jane Austen",
     firstFalseAnswer: "J.K. Rowling",
+    answer: "Jane Austen",
     secondFalseAnswer: "Charlote Bronte",
 
-    //function
-        //if (userGuess == answer) {
-            //console.log("Correct!");
-        //} else if(user guess == firstFalseAnswer || secondFalseAnswer){
-            //console.log("Wrong!");
-        //} else {
+    firstPossibility: function(){
+        if (userGuess == answer) {
+            console.log("Correct!");
+        } else if(userGuess == firstFalseAnswer || secondFalseAnswer){
+            console.log("Wrong!");
+        } else {
             //timer gets to 0, goes to answer page automatically
-            //console.log("sorry, time's up");
-        //}
+            console.log("sorry, time's up");
+        }
+    }
 };
-    console.log(firstQuestion.question1);
-    console.log(firstQuestion.answer);
+$("#first-answer").html(firstQuestion.firstFalseAnswer);
+$("#second-answer").html(firstQuestion.answer);
+$("#third-answer").html(firstQuestion.secondFalseAnswer);
+    //console.log(firstQuestion.question1);
+    //console.log(firstQuestion.answer);
 
 var secondQuestion = {
     question2: "Which Charles Dickens work has over 100 movie adaptations?",
-    answer: "A Christmas Carol",
-    firstFalseAnswer: "Oliver Twist",
     secondFalseAnswer: "Great Expectations",
+    firstFalseAnswer: "Oliver Twist",
+    answer: "A Christmas Carol",
+
     //function
         //if (userGuess == answer) {
             //console.log("Correct!");
@@ -39,14 +50,14 @@ var secondQuestion = {
             //console.log("sorry, time's up");
         //}
 };
-    console.log(secondQuestion.question2);
-    console.log(secondQuestion.firstFalseAnswer);
+    //console.log(secondQuestion.question2);
+    //console.log(secondQuestion.firstFalseAnswer);
 
 var thirdQuestion = {
     question3: "When was the first Harry Potter book published?",
     answer: "1997",
-    firstFalseAnswer: "2001",
     secondFalseAnswer: "2010",
+    firstFalseAnswer: "2001",
     //function
         //if (userGuess == answer) {
             //console.log("Correct!");
@@ -57,13 +68,14 @@ var thirdQuestion = {
             //console.log("sorry, time's up");
         //}
 };
-    console.log(thirdQuestion.question3);
-    console.log(thirdQuestion.secondFalseAnswer);
+    //console.log(thirdQuestion.question3);
+    //console.log(thirdQuestion.secondFalseAnswer);
 var fourthQuestion = {
     question4: "What is Florida author Zora Neale Hurston\s most famous work?",
+    secondFalseAnswer: "Mules and Men",
     answer: "Their Eyes Were Watching God",
     firstFalseAnswer: "Jonah's Gourd Vine",
-    secondFalseAnswer: "Mules and Men",
+
     //function
         //if (userGuess == answer) {
             //console.log("Correct!");
@@ -74,7 +86,23 @@ var fourthQuestion = {
             //console.log("sorry, time's up");
         //}
 }
-console.log(fourthQuestion.question4);
-console.log(fourthQuestion.answer);
-console.log(fourthQuestion.firstFalseAnswer);
-console.log(fourthQuestion.secondFalseAnswer);
+//console.log(fourthQuestion.question4);
+//console.log(fourthQuestion.answer);
+//console.log(fourthQuestion.firstFalseAnswer);
+//console.log(fourthQuestion.secondFalseAnswer);
+
+$("#first-answer").on('click', function(){
+    console.log('That is your guess');
+});
+
+$('#second-answer').on('click', function(){
+    console.log('That is your second guess');
+
+});
+
+$('#third-answer').on('click', function(){
+    console.log('That is your third guess');
+
+});
+
+});
