abaa007346fe0c3884b56aece864042cec5300be
Ciarra Murphy <mciarra@ymail.com>
2017-08-30 21:39:17 -0400

    added array to push objects to and added an index to grab them when a button is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,101 +8,122 @@ $(document).ready(function(){
 var win = 0;
 var loss = 0;
 var unanswered = 0;
-var userGuess;
+var index = 0;
+var arrayOfQuestions = [];
+var endOfGame = 4;
 
 //Questions
 var firstQuestion = {
-    question1: "Who wrote Pride and Prejudice?",
-    firstFalseAnswer: "J.K. Rowling",
-    answer: "Jane Austen",
-    secondFalseAnswer: "Charlote Bronte",
-
-    firstPossibility: function(){
-        if (userGuess == answer) {
-            console.log("Correct!");
-        } else if(userGuess == firstFalseAnswer || secondFalseAnswer){
-            console.log("Wrong!");
-        } else {
-            //timer gets to 0, goes to answer page automatically
-            console.log("sorry, time's up");
-        }
-    }
-};
-$("#first-answer").html(firstQuestion.firstFalseAnswer);
-$("#second-answer").html(firstQuestion.answer);
-$("#third-answer").html(firstQuestion.secondFalseAnswer);
-    //console.log(firstQuestion.question1);
-    //console.log(firstQuestion.answer);
-
+    question: "Who wrote Pride and Prejudice?",
+    firstAnswer: "J.K. Rowling",
+    secondAnswer: "Jane Austen",
+    thirdAnswer: "Charlote Bronte",
+    correct: 2,
+}
 var secondQuestion = {
-    question2: "Which Charles Dickens work has over 100 movie adaptations?",
-    secondFalseAnswer: "Great Expectations",
-    firstFalseAnswer: "Oliver Twist",
-    answer: "A Christmas Carol",
-
-    //function
-        //if (userGuess == answer) {
-            //console.log("Correct!");
-        //} else if(user guess == firstFalseAnswer || secondFalseAnswer){
-            //console.log("Wrong!");
-        //} else {
-            //timer gets to 0, goes to answer page automatically
-            //console.log("sorry, time's up");
-        //}
+    question: "Which Charles Dickens work has over 100 movie adaptations?",
+    firstAnswer: "Great Expectations",
+    secondAnswer: "Oliver Twist",
+    thirdAnswer: "A Christmas Carol",
+    correct: 3,
 };
+var thirdQuestion = {
+    question: "When was the first Harry Potter book published?",
+    firstAnswer: "1997",
+    secondAnswer: "2010",
+    thirdAnswer: "2001",
+    correct: 1,
+};
+var fourthQuestion = {
+    question: "What is Florida author Zora Neale Hurston\s most famous work?",
+    firstAnswer: "Mules and Men",
+    secondAnswer: "Their Eyes Were Watching God",
+    thirdAnswer: "Jonah's Gourd Vine",
+    correct: 2,
+}
+
+arrayOfQuestions.push(firstQuestion);
+arrayOfQuestions.push(secondQuestion);
+arrayOfQuestions.push(thirdQuestion);
+arrayOfQuestions.push(fourthQuestion);
+
+function myQuestions(){
+$("#question").html(arrayOfQuestions[index].question);
+$("#first-answer").html(arrayOfQuestions[index].firstAnswer);
+$("#second-answer").html(arrayOfQuestions[index].secondAnswer);
+$("#third-answer").html(arrayOfQuestions[index].thirdAnswer);
+}
+
+myQuestions();
+
+//on timer
+// incr loss
+// incr index , test for end of game 
+
+$("#first-answer").on('click', function(){
+    //console.log('That is your guess');
+    if(arrayOfQuestions[index].correct == 1){
+        console.log('good');
+    } else{
+        console.log('bad');
+    }
+    //index increment
+    index++;
+if(index == endOfGame){
+    index = 0;
+}
+    
+
+    myQuestions();
+
+    
+    // if the index == end_of_game then call end function
+    // else call function that displays Q and answer
+    //$("#first-answer").html(firstAnswers[index]);
+});
+
+$('#second-answer').on('click', function(){
+    //console.log('That is your second guess');
+    if(arrayOfQuestions[index].correct == 2){
+        console.log('good');
+    } else{
+        console.log('bad');
+    }
+    index++;
+    if(index == endOfGame){
+        index = 0;
+    }
+    myQuestions();
+    
+});
+
+$('#third-answer').on('click', function(){
+    //console.log('That is your third guess');
+    if(arrayOfQuestions[index].correct == 3){
+        console.log('good');
+    } else{
+        console.log('bad');
+    }
+    index++;
+    if(index == endOfGame){
+        index = 0;
+    }
+    myQuestions();
+    
+});
+
+
     //console.log(secondQuestion.question2);
     //console.log(secondQuestion.firstFalseAnswer);
 
-var thirdQuestion = {
-    question3: "When was the first Harry Potter book published?",
-    answer: "1997",
-    secondFalseAnswer: "2010",
-    firstFalseAnswer: "2001",
-    //function
-        //if (userGuess == answer) {
-            //console.log("Correct!");
-        //} else if(user guess == firstFalseAnswer || secondFalseAnswer){
-            //console.log("Wrong!");
-        //} else {
-            //timer gets to 0, goes to answer page automatically
-            //console.log("sorry, time's up");
-        //}
-};
+
     //console.log(thirdQuestion.question3);
     //console.log(thirdQuestion.secondFalseAnswer);
-var fourthQuestion = {
-    question4: "What is Florida author Zora Neale Hurston\s most famous work?",
-    secondFalseAnswer: "Mules and Men",
-    answer: "Their Eyes Were Watching God",
-    firstFalseAnswer: "Jonah's Gourd Vine",
 
-    //function
-        //if (userGuess == answer) {
-            //console.log("Correct!");
-        //} else if(user guess == firstFalseAnswer || secondFalseAnswer){
-            //console.log("Wrong!");
-        //} else {
-            //timer gets to 0, goes to answer page automatically
-            //console.log("sorry, time's up");
-        //}
-}
 //console.log(fourthQuestion.question4);
 //console.log(fourthQuestion.answer);
 //console.log(fourthQuestion.firstFalseAnswer);
 //console.log(fourthQuestion.secondFalseAnswer);
 
-$("#first-answer").on('click', function(){
-    console.log('That is your guess');
-});
-
-$('#second-answer').on('click', function(){
-    console.log('That is your second guess');
-
-});
-
-$('#third-answer').on('click', function(){
-    console.log('That is your third guess');
-
-});
 
 });
