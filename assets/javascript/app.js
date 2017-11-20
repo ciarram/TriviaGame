63d8381790ed76cb15733c34eeb1beabdbeaa5eb
Ciarra Murphy <mciarra@ymail.com>
2017-11-19 23:49:20 -0500

    Added questions and answers to the arrays and took out objects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,76 +36,76 @@ var win = 0;
 var loss = 0;
 var unanswered = 0;
 var index = 0;
-var arrayOfQuestions = [];
-var correct = [];
-var firstIncorrect = [];
-var secondIncorrect = [];
+var arrayOfQuestions = ["Who wrote Pride and Prejudice?", "Which Charles Dickens work has over 100 movie adaptations?", "When was the first Harry Potter book published?", "What is Florida author Zora Neale Hurston\s most famous work?"];
+var correct = ["Jane Austen", "A Christmas Carol", "1997", "Their Eyes Were Watching God"];
+var firstIncorrect = ["J.K. Rowling", "Great Expectations", "2010", "Mules and Men"];
+var secondIncorrect = ["Charlote Bronte", "Oliver Twist", "2001", "Jonah's Gourd Vine"];
 var endOfGame = 4;
 
 //Questions
-var firstQuestion = {
-    question: "Who wrote Pride and Prejudice?",
-    incorrectAnswer: "J.K. Rowling",
-    correctAnswer: "Jane Austen",
-    secondIncorrectAnswer: "Charlote Bronte",
-}
-var secondQuestion = {
-    question: "Which Charles Dickens work has over 100 movie adaptations?",
-    incorrectAnswer: "Great Expectations",
-    secondIncorrectAnswer: "Oliver Twist",
-    correctAnswer: "A Christmas Carol",
-};
-var thirdQuestion = {
-    question: "When was the first Harry Potter book published?",
-    correctAnswer: "1997",
-    incorrectAnswer: "2010",
-    secondIncorrectAnswer: "2001",
-};
-var fourthQuestion = {
-    question: "What is Florida author Zora Neale Hurston\s most famous work?",
-    incorrectAnswer: "Mules and Men",
-    correctAnswer: "Their Eyes Were Watching God",
-    secondIncorrectAnswer: "Jonah's Gourd Vine",
-}
-
-arrayOfQuestions.push(firstQuestion.question);
-arrayOfQuestions.push(secondQuestion.question);
-arrayOfQuestions.push(thirdQuestion.question);
-arrayOfQuestions.push(fourthQuestion.question);
-console.log(arrayOfQuestions);
-
-correct.push(firstQuestion.correctAnswer);
-correct.push(secondQuestion.correctAnswer);
-correct.push(thirdQuestion.correctAnswer);
-correct.push(fourthQuestion.correctAnswer);
-console.log(correct);
-
-firstIncorrect.push(firstQuestion.incorrectAnswer);
-firstIncorrect.push(secondQuestion.incorrectAnswer);
-firstIncorrect.push(thirdQuestion.incorrectAnswer);
-firstIncorrect.push(fourthQuestion.incorrectAnswer);
-console.log(firstIncorrect);
-
-secondIncorrect.push(firstQuestion.secondIncorrectAnswer);
-secondIncorrect.push(secondQuestion.secondIncorrectAnswer);
-secondIncorrect.push(thirdQuestion.secondIncorrectAnswer);
-secondIncorrect.push(fourthQuestion.secondIncorrectAnswer);
-console.log(secondIncorrect);
-
-// function myQuestions(){
-// $("#question").append(arrayOfQuestions);
-// $("#first-answer").append(correct);
-// $("#second-answer").append(firstIncorrect);
-// $("#third-answer").append(secondIncorrect);
+// var firstQuestion = {
+//     question: "Who wrote Pride and Prejudice?",
+//     incorrectAnswer: "J.K. Rowling",
+//     correctAnswer: "Jane Austen",
+//     secondIncorrectAnswer: "Charlote Bronte",
+// }
+// var secondQuestion = {
+//     question: "Which Charles Dickens work has over 100 movie adaptations?",
+//     incorrectAnswer: "Great Expectations",
+//     secondIncorrectAnswer: "Oliver Twist",
+//     correctAnswer: "A Christmas Carol",
+// };
+// var thirdQuestion = {
+//     question: "When was the first Harry Potter book published?",
+//     correctAnswer: "1997",
+//     incorrectAnswer: "2010",
+//     secondIncorrectAnswer: "2001",
+// };
+// var fourthQuestion = {
+//     question: "What is Florida author Zora Neale Hurston\s most famous work?",
+//     incorrectAnswer: "Mules and Men",
+//     correctAnswer: "Their Eyes Were Watching God",
+//     secondIncorrectAnswer: "Jonah's Gourd Vine",
 // }
 
-// myQuestions();
+// arrayOfQuestions.push(firstQuestion.question);
+// arrayOfQuestions.push(secondQuestion.question);
+// arrayOfQuestions.push(thirdQuestion.question);
+// arrayOfQuestions.push(fourthQuestion.question);
+// console.log(arrayOfQuestions);
+
+// correct.push(firstQuestion.correctAnswer);
+// correct.push(secondQuestion.correctAnswer);
+// correct.push(thirdQuestion.correctAnswer);
+// correct.push(fourthQuestion.correctAnswer);
+// console.log(correct);
+
+// firstIncorrect.push(firstQuestion.incorrectAnswer);
+// firstIncorrect.push(secondQuestion.incorrectAnswer);
+// firstIncorrect.push(thirdQuestion.incorrectAnswer);
+// firstIncorrect.push(fourthQuestion.incorrectAnswer);
+// console.log(firstIncorrect);
+
+// secondIncorrect.push(firstQuestion.secondIncorrectAnswer);
+// secondIncorrect.push(secondQuestion.secondIncorrectAnswer);
+// secondIncorrect.push(thirdQuestion.secondIncorrectAnswer);
+// secondIncorrect.push(fourthQuestion.secondIncorrectAnswer);
+// console.log(secondIncorrect);
+
+function myQuestions(){
+$("#question").html(arrayOfQuestions);
+$("#first-answer").html(correct);
+$("#second-answer").html(firstIncorrect);
+$("#third-answer").html(secondIncorrect);
+}
+
+myQuestions();
 
 //on timer
 // incr loss
 // incr index , test for end of game 
 
-$("#correct-answer").on('click', function(){
+$("#first-answer").on('click', function(){
     //console.log('That is your guess');
     if(arrayOfQuestions[index].correct == 1){
         console.log('correct');
